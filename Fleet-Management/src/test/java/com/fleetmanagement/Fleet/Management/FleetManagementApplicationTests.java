68e1bb5ff3c9dbbb9a7b5e952d35fd2de99852cb
javafullstackD <javafullstackdeveloper705@gmail.com>
2022-07-16 21:30:43 +0200

    created the project structure and configured the db

---
+++ Fleet-Management/src/test/java/com/fleetmanagement/Fleet/Management/FleetManagementApplicationTests.java
@@ -0,0 +1,13 @@
+package com.fleetmanagement.Fleet.Management;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FleetManagementApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
